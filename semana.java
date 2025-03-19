d3b2b4bbb0e088be93d9859d60ed5392de777bbd
Hugoleonardo1 <hugoleonardogomes12@gmail.com>
2025-03-19 14:22:18 -0300

    semana.java

--- semana.java
+++ semana.java
@@ -7,42 +7,38 @@ public class semana {
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in);
-		
-		int diasemana = 1, 2, 3, 4, 5, 6, 7;
-		int domingo = 1;
-		int segunda = 2;
-		int terca = 3;
-		int quarta = 4;
-		int quinta = 5;
-		int sexta = 6;
-		int sabado = 7;
-		
-		
-		System.out.println("Informe o número do dia da semana: ");
-		int diasemana = ler.nextInt;
-		
-		if (diasemana = 1) {
-			System.out.println("Domingo.");
-		}
-		else if (diasemana = 2) {
-			System.out.println("Segunda-feira.");                         
-		}
-		else if (diasemana = 3) {
-			System.out.println("Terça-feira.");
-		}
-		else if (diasemana = 4) {
-			System.out.println("quarta-feira.");
-		}
-		else if (diasemana = 5) {
-			System.out.println("quinta-feira.");
-		}
-		else if (diasemana = 6) {
-			System.out.println("sexta-feira.");
-		}
-		else (diasemana = 7) {
-			System.out.println("sábado.");
-		}
-
+	
+	int diasemana;
+	
+	
+	System.out.println("Informe o número do dia da semana: ");
+	diasemana = ler.nextInt();
+	
+	if (diasemana == 1) {
+		System.out.println("Domingo.");
 	}
+	else if (diasemana == 2) {
+		System.out.println("Segunda-feira.");                         
+	}
+	else if (diasemana == 3) {
+		System.out.println("Terça-feira.");
+	}
+	else if (diasemana == 4) {
+		System.out.println("quarta-feira.");
+	}
+	else if (diasemana == 5) {
+		System.out.println("quinta-feira.");
+	}
+	else if (diasemana == 6) {
+		System.out.println("sexta-feira.");
+	}
+	else {
+		System.out.println("sábado.");
+	}
+	
+	ler.close();
+	
+	}
+}
 
 }
