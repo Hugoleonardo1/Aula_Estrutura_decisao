7b26eac590e359baeddbf53913a76f9b769c68a7
Hugoleonardo1 <hugoleonardogomes12@gmail.com>
2025-03-19 14:11:54 -0300

    desafio_par_ou_impar.java

--- desafio_par_ou_impar.java
+++ desafio_par_ou_impar.java
@@ -13,14 +13,17 @@ public class desafio_par_ou_impar {
 		System.out.print("Informe um número inteiro: ");
 		numero = ler.nextInt();
 		
-		if numero (% 2 ==0) {
-			Sytem.out.println("O número é par.");
+		if (numero % 2 == 0) {
+			System.out.print("O número é par.");
 		}
 		else {
-			System.out.println("O número é impar.");
+			System.out.print("O número é impar.");
 		}
+		ler.close();
 		
-		}
-		}
+	}
+
+	}
+		
 		
 		
