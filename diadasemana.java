bb6bc52b7fba6f4016653d16582ef8c03880c4b6
Hugoleonardo1 <hugoleonardogomes12@gmail.com>
2025-03-19 14:17:36 -0300

    diadasemana.java

--- diadasemana.java
+++ diadasemana.java
@@ -8,49 +8,36 @@ public class diadasemana {
 		
 	Scanner ler = new Scanner(System.in);
 	
-	int diasemana = 1, 2, 3, 4, 5, 6, 7;
-	int domingo = 1;
-	int segunda = 2;
-	int terca = 3;
-	int quarta = 4;
-	int quinta = 5;
-	int sexta = 6;
-	int sabado = 7;
+	int diasemana;
 	
 	
 	System.out.println("Informe o número do dia da semana: ");
-	int diasemana = ler.nextInt;
+	diasemana = ler.nextInt();
 	
-	if (diasemana = 1) {
+	if (diasemana == 1) {
 		System.out.println("Domingo.");
 	}
-	else if (diasemana = 2) {
+	else if (diasemana == 2) {
 		System.out.println("Segunda-feira.");                         
 	}
-	else if (diasemana = 3) {
+	else if (diasemana == 3) {
 		System.out.println("Terça-feira.");
 	}
-	else if (diasemana = 4) {
+	else if (diasemana == 4) {
 		System.out.println("quarta-feira.");
 	}
-	else if (diasemana = 5) {
+	else if (diasemana == 5) {
 		System.out.println("quinta-feira.");
 	}
-	else if (diasemana = 6) {
+	else if (diasemana == 6) {
 		System.out.println("sexta-feira.");
 	}
-	else (diasemana = 7) {
+	else {
 		System.out.println("sábado.");
 	}
 	
-	ler.closed();
-		
+	ler.close();
+	
 	}
-	}
-	
-	
-	
-	
-
-	}
+}
 
