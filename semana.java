7ee8e3c90af3de759645d99cbf5101271b985dac
Hugoleonardo1 <hugoleonardogomes12@gmail.com>
2025-04-02 10:31:16 -0300

    semana.java

--- semana.java
+++ semana.java
@@ -7,38 +7,38 @@ public class semana {
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in);
-	
-	int diasemana;
-	
-	
-	System.out.println("Informe o número do dia da semana: ");
-	diasemana = ler.nextInt();
-	
-	if (diasemana == 1) {
-		System.out.println("Domingo.");
+		
+		int diasemana;
+		
+		System.out.println("Informe o número do dia da semana: ");
+		diasemana = ler.nextInt();
+		
+		if (diasemana == 1) {
+			System.out.print("Domingo.");
+		}
+		else if (diasemana == 2) {
+			System.out.println("Segunda-feira.");                         
+		}
+		else if (diasemana == 3) {
+			System.out.println("Terça-feira.");
+		}
+		else if (diasemana == 4) {
+			System.out.println("quarta-feira.");
+		}
+		else if (diasemana == 5) {
+			System.out.println("quinta-feira.");
+		}
+		else if (diasemana == 6) {
+			System.out.println("sexta-feira.");
+		}
+		else if (diasemana == 7) {
+			System.out.println("sábado.");
+		}
+		else {
+			System.out.print("O número digitado é inválido");
+		}
+           ler.close();
 	}
-	else if (diasemana == 2) {
-		System.out.println("Segunda-feira.");                         
-	}
-	else if (diasemana == 3) {
-		System.out.println("Terça-feira.");
-	}
-	else if (diasemana == 4) {
-		System.out.println("quarta-feira.");
-	}
-	else if (diasemana == 5) {
-		System.out.println("quinta-feira.");
-	}
-	else if (diasemana == 6) {
-		System.out.println("sexta-feira.");
-	}
-	else {
-		System.out.println("sábado.");
-	}
-	
-	ler.close();
-	
-	}
-}
 
 }
+
